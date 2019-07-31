deb402c2bda6a4921c4aad6d5fd91fb667bbc255
surimsohn <surim.sohn@gmail.com>
2019-08-01 00:00:22 +0900

    data_trans

--- puzzle.py
+++ puzzle.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
-puzzle_df = pd.read_csv('data/puzzle.csv',index_col=0)
+puzzle_df = pd.read_csv('data/puzzle.csv', index_col=0)
 
-puzzle_df['A'] = puzzle_df['A']*2
-puzzle_df.iloc[2,5] = 99
-puzzle_df[puzzle_df.loc[:,'B':'E'] >= 80] =1
+puzzle_df['A'] = puzzle_df['A'] * 2
+puzzle_df.iloc[2, 'F'] = 99
+puzzle_df[puzzle_df.loc[:, 'B':'E'] < 80] = 0
+puzzle_df[puzzle_df.loc[:, 'B':'E'] >= 80] = 1
+
 
 print(puzzle_df)
+
+
